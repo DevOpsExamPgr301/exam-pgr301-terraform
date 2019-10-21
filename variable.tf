d73b17b66c16bc540b19edc52e667c88a4b4fc1e
anonymous <anonymous>
2019-10-21 12:40:45 +0200

    initial commit

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "name" {
+  description = "Base name for the Heroku app"
+}
+
+variable "buildpacks" {
+  description = "Array of heroku buildpacks"
+}
+
+variable "region" {
+  description = "The region for Heroku deploy"
+  default = "eu"
+}
