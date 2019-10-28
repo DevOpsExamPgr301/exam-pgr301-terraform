fc6540152964942a6c93bda558aae9e3c2f15331
Kjell-Olaf Slagnes <kjellolaf@lucalabs.com>
2019-10-28 10:36:18 +0100

    Fix state, fix heroku config vars

--- app.tf
+++ app.tf
@@ -10,8 +10,18 @@ resource "heroku_app" "production" {
   buildpacks = "${var.buildpacks}"
 }
 
-resource "heroku_config" "endpoints" {
-  vars = {
+resource "heroku_app_config_association" "staging" {
+  app_id = "${heroku_app.staging.id}"
+
+  sensitive_vars = {
+    LOGZ_IO_TOKEN = "${var.logz_io_token}"
+  }
+}
+
+resource "heroku_app_config_association" "production" {
+  app_id = "${heroku_app.production.id}"
+
+  sensitive_vars = {
     LOGZ_IO_TOKEN = "${var.logz_io_token}"
   }
 }
